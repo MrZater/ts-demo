c5fc9dcb5a2030c592e0a7204c85db8e1966c297
zhoutao <mrzater@163.com>
2023-05-12 16:20:32 +0800

    0512

---
+++ src/index copy类型.ts
@@ -0,0 +1,193 @@
+// let a = 3 + 1;
+// let b = 3 + 3;
+// const obj = {
+//   bar: a,
+//   foo: b,
+// };
+// function squareOf(n: number): number {
+//   return n * n;
+// }
+// console.log(squareOf(3));
+
+
+// function test(n: number | string) {
+//   return n;
+// }
+// console.log(test(3));
+
+// any
+// const a: any = 3;
+// const b: any = [123, 555];
+// const c = a + b;
+// console.log(c);
+
+// unknown
+// const a: unknown = 30;
+// const b: boolean = a === 30;
+// if (typeof a === "number") {
+//   const d = a + 10;
+//   console.log(d);
+// }
+
+// boolean
+// let a: true = true;
+// let b: boolean = false;
+
+// number
+// let a: 123 = 123;
+// let b: number = 123;
+
+// bigint
+// let a = 313123n;
+// let b: bigint = 3n;
+
+// object
+// const a: { test: number; c: string } = {
+//   test: 123,
+//   c: "123",
+// };
+// console.log(a.test);
+
+// class
+// class Person {
+//   constructor(public name: string, public age: number, public email?: string) { }
+// }
+// const xiaomin = new Person("xiaomin", 18, 'mrzater@163.com');
+// console.log(xiaomin)
+
+// [key: T]:U 索引签名
+// const a: {
+//   b: string
+//   c?: number
+//   [key: number]: number
+// } = {
+//   b: 'test',
+//   c: 1,
+//   4: 3
+// }
+// console.log(a);
+
+// readonly 只读
+// const a: {
+//   readonly b: string
+// } = {
+//   b: 'test'
+// }
+// console.log(a);
+
+
+// 类型别名
+// type Age = number
+// type Name = string
+// type Person = {
+//   name: Name,
+//   age: Age
+// }
+
+// let driver: Person = {
+//   name: 'jack',
+//   age: 18
+// }
+
+// // 并集类型和交集类型
+// type Person1 = {
+//   name: string,
+//   age: number,
+//   email: string
+// }
+// type Person2 = {
+//   name: string,
+//   age: number,
+//   address: string
+// }
+// // User中兼合Person1 Person2
+// type User1 = Person1 & Person2
+// // User中兼合Person1 Person2共有部分
+// type User2 = Person1 | Person2
+// const user1: User1 = {
+//   name: 'jack',
+//   age: 18,
+//   email: 'mrzater@163.com',
+//   address: '北京'
+// } 
+// const user2: User2 = {
+//   name: 'jack',
+//   age: 18,
+//   email: 'mrzater@163.com',
+//   address: '北京',
+
+// } 
+// console.log(user1, user2);
+
+
+// type Returns = true | null
+// function trueOrNull(isTrue: boolean): Returns {
+//   if (isTrue) {
+//     return true
+//   }
+//   return null
+// }
+
+// 数组
+// const a: number[] = [1, 2, 3]
+// const b = [123]
+// b.push(1)
+// let c: number[] = []
+// function getArray() {
+//   const arr = [] // any
+//   arr.push(1)
+//   arr.push('test')
+//   return arr
+// }
+// const newarr = getArray() // (number | stringß)[]
+
+
+// // 元组
+// // 数组固定长度，每项类型
+// const a: [string] = ['abc']
+// const b: [string, number] = ['abc', 1]
+// // 可选
+// const c: [string, number?] = ['abc']
+// // 嵌套数组
+// const d: [string, number][] = [['abc', 13], ['test', 1]]
+// // 支持剩余参数
+// const e: [string, ...number[]] = ['abc', 3]
+
+
+// // 只读数组的三种写法
+// type A = readonly string[]
+// type B = ReadonlyArray<string>
+// type C = Readonly<string[]>
+
+
+// // 枚举
+// enum Student {
+//   Jack, // 0
+//   Mary, // 1
+//   Tom, // 2
+//   Jarry = 100, // 100 下一项 +1 
+//   Mark, // 101
+//   Anna = '102' // string和number混用
+// }
+
+// const Jack = Student[0]
+// const Tom = Student.Tom
+// const Mark = Student.Mark
+// const Anna = Student['Anna'] // 通过键访问
+// console.log(Tom, Mark, Anna);
+
+// // const enum
+// // const enum 不允许获取不存在的项，不允许反向查找
+// const enum Person {
+//   Jack,
+//   Mary,
+//   Tom,
+//   Jarry,
+//   Mark,
+//   Anna
+// }
+// function getUser(uid: Person) {
+//   return uid
+// }
+// const u = getUser(Person.Jack)
+// console.log(u)  
--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,199 @@
-// let a = 3 + 1;
-// let b = 3 + 3;
-// const obj = {
-//   bar: a,
-//   foo: b,
-// };
-// function squareOf(n: number): number {
-//   return n * n;
+// // 函数
+// function getA(a: number) {
+//   return a
 // }
-// console.log(squareOf(3));
-
-// function test(n: number | string) {
-//   return n;
+// const getB = function (b: number) {
+//   return b
 // }
-// console.log(test(3));
+// const getC = (c: number) => {
+//   return c
+// }
+// const getD = (d: number) => d
 
-// any
-// const a: any = 3;
-// const b: any = [123, 555];
-// const c = a + b;
-// console.log(c);
+// // 可选和默认传参
+// function test1(a: number, b?: string) {
+// }
+// test1(1, 'a')
+// function test2(a: number, b: string = 'abc') {
 
-// unknown
-// const a: unknown = 30;
-// const b: boolean = a === 30;
-// if (typeof a === "number") {
-//   const d = a + 10;
-//   console.log(d);
+// }
+// test2(1)
+// type Content = {
+//   text1?: string,
+//   text2?: string
+// }
+// function test3(a: number, content: Content = {}) {
+
+// }
+// test3(1, { text1: 'a', text2: 'b' })
+
+
+// // 剩余参数
+// // 数组传参
+// function sum1(numbers: number[]): number {
+//   return numbers.reduce((sum, item) => sum + item)
+// }
+// console.log(sum1([1, 2, 3, 4, 5, 6, 7]));
+
+// // 非数组传参
+// function sum2(...numbers: number[]) {
+//   return numbers.reduce((sum, item) => sum + item)
+// }
+// console.log(sum2(1, 2, 3, 4, 5, 6, 7, 8));
+
+// // call apply bind
+// function add(a: number, b: number) {
+//   return a + b
+// }
+// console.log(add(1, 2));
+// console.log(add.apply(null, [1, 2]));
+// console.log(add.call(null, 1, 2));
+// console.log(add.bind(null, 1, 2)());
+
+// // 注解this的类型
+// function fancyDate(this:Date) {
+//   return `${this.getDate()}/${this.getMonth()}/${this.getFullYear()}`
 // }
 
-// boolean
-// let a: true = true;
-// let b: boolean = false;
+// console.log(fancyDate.call(new Date()));
 
-// number
-// let a: 123 = 123;
-// let b: number = 123;
+// 调用签名
+// type Sum = (a: number, b: number) => number
+// const sum: Sum = (c, d) => {
+//   return c + d
+// }
+// console.log(sum(1, 9));
 
-// bigint
-// let a = 313123n;
-// let b: bigint = 3n;
 
-// object
-// const a: { test: number; c: string } = {
-//   test: 123,
-//   c: "123",
-// };
-// console.log(a.test);
-class Person {
-  constructor(public name: string, public age: number) {}
+// 上下文类型推导
+// function times(
+//   func: (index: number) => void,
+//   number: number
+// ) {
+//   for (let i = 0; i < number; i++) {
+//     func(i)
+//   }
+// }
+
+// times((i) => console.log(i), 10)
+
+// 函数类型重载
+// type Reserve = {
+//     (from: Date, to: Date, destination: string): void
+//     (from: Date, destination: string): void
+// }
+
+// const reserve: Reserve = (
+//     from: Date,
+//     toOrDestination: Date | string,
+//     destination?: string
+// ) => {
+//     // ...
+// }
+
+
+// type User = {
+//     name: string,
+//     age: number,
+//     email: string,
+//     address: string
+//     sex?: boolean
+// }
+// type Person = {
+//     (name: string, age: number, email: string, address: string): User
+//     (name: string, age: number, email: string, address: string, boolean?: boolean): User
+// }
+// const person: Person = (
+//     name,
+//     age,
+//     email,
+//     address,
+//     sex?: boolean
+// ) => {
+//     return {
+//         name, age, email, address, sex
+//     }
+// }
+
+
+
+// type WarnUser = {
+//     // 函数传参
+//     (warning: string): void,
+//     // 函数属性
+//     wasCalled: boolean
+// }
+
+// const warnUser: WarnUser = (warning: string) => {
+//     if (warnUser.wasCalled) {
+//         return
+//     }
+//     warnUser.wasCalled = true
+//     alert(true)
+// }
+// warnUser.wasCalled = false
+// warnUser('warning')
+
+
+// 泛型
+// type Filter = {
+//     (arr: number[], func: (item: number, i?: number) => boolean): number[]
+//     (arr: string[], func: (item: string, i?: number) => boolean): string[]
+//     (arr: object[], func: (item: object, i?: number) => boolean): object[]
+// }
+// type Filter = {
+//     <T>(arr: T[], func: (item: T, i?: number) => boolean): T[]
+// }
+// const myFilter: Filter = (arr, func) => {
+//     let result = []
+//     for (let i = 0; i < arr.length; i++) {
+//         let item = arr[i]
+//         if (func(item, i)) {
+//             result.push(item)
+//         }
+//     }
+//     return result
+// }
+// const newArr1 = myFilter([1, 2, 4, 5, 6, 67], (item) => item > 5)
+// const newArr2 = myFilter(['1', 'abc', 'ccc', 'as', 'js', 'ts', 6], (item) => item === 'js')
+// console.log(newArr1, newArr2);
+
+// 泛型的使用
+// type User = {
+//     <T>(name: T, age: number, email: T): void
+// }
+// const user: User = (name, age, email) => {
+// }
+// user('jack', 18, 'www.baidu.com')
+
+// type MyMap = {
+//     <T, U>(arr: T[], func: (item: T, index?: number) => U): U[]
+// }
+// const myMap: MyMap = (arr, func) => {
+//     let result = []
+//     for (let i = 0; i < arr.length; i++) {
+//         let item = arr[i]
+//         result[i] = func(item, i)
+//     }
+//     return result
+// }
+// const newArr1 = myMap([1, 2, 3, 4], (item, i) => item + 3)
+// console.log(newArr1);
+
+// 泛型推导
+type MyMap = {
+    <T, U>(arr: T[], func: (item: T, index?: number) => U): U[]
 }
-const xiaomin = new Person("xiaomin", 18);
-console.log(xiaomin)
+const myMap: MyMap = (arr, func) => {
+    let result = []
+    for (let i = 0; i < arr.length; i++) {
+        let item = arr[i]
+        result[i] = func(item, i)
+    }
+    return result
+}
+
+const newArr1 = myMap<number, number>([1, 2, 3, 4], (item, i) => item + 3)// 泛型传参
+console.log(newArr1);
+
+
